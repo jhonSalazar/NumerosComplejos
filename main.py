1d8c3e755caea320402950266f8ca25087cd4fdf
Jhon Salazar <farsalazar@gmail.com>
2019-04-17 23:10:37 -0300

    se agrega estructura del proyecto

---
+++ main.py
@@ -0,0 +1,14 @@
+import cmath 
+  
+# Prueba de script, inicializando un numero complejo
+x = 5
+y = 3
+
+z = complex(x,y); # creamos un numero complejo
+  
+print("Numero compleo: ",z)
+print ("Parte Real:",end="") 
+print (z.real) 
+
+print ("Parte imaginaria:",end="") 
+print (z.imag)
