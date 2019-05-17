d6e6a0f0113e64bbc54746f9ed5547ad99edcc04
leonelMoyano <leonel.alejandro.moyano@gmail.com>
2019-05-17 01:17:50 -0300

    Operaciones complejas sin tests

--- paquetes/operaciones.py
+++ paquetes/operaciones.py
@@ -1,30 +1,43 @@
 import math
-from paquetes import complejo
+from paquetes.complejo import Complejo
 
 class Operaciones:
 
     @classmethod
-    def suma(cls, complejo1, complejo2):
+    def suma( cls, complejo1, complejo2 ):
         real = complejo1.real + complejo2.real
         imginaria = complejo1.img + complejo2.img
-        return complejo.Complejo(real,imginaria)
+        return Complejo( real, imginaria )
 
     @classmethod
-    def resta(cls, complejo1, complejo2):
+    def resta( cls, complejo1, complejo2 ):
         real = complejo1.real - complejo2.real
         imginaria = complejo1.img - complejo2.img
-        return complejo.Complejo(real, imginaria)
+        return Complejo( real, imginaria )
 
     @classmethod
-    def multiplicacion(cls, complejo1, complejo2):
+    def multiplicacion( cls, complejo1, complejo2 ):
         modulo = complejo1.modulo() * complejo2.modulo()
         argumento = complejo1.argumento() + complejo2.argumento()
-        return complejo.Complejo.CrearFormaPolar(modulo,argumento)
+        return Complejo.CrearFormaPolar( modulo,argumento )
 
     @classmethod
-    def division(cls, complejo1, complejo2):
+    def division( cls, complejo1, complejo2 ):
         modulo = complejo1.modulo() / complejo2.modulo()
         argumento = complejo1.argumento() - complejo2.argumento()
-        return complejo.Complejo.CrearFormaPolar(modulo, argumento)
+        return Complejo.CrearFormaPolar( modulo, argumento )
 
+    @classmethod
+    def potencia( cls, complejo, exponente ):
+        modulo = complejo.modulo() ** exponente
+        argumento = complejo.argumento() * exponente
+        return Complejo.CrearFormaPolar( modulo, argumento )
 
+    @classmethod
+    def raices( cls, complejo, exponente ):
+        modulo = complejo.modulo() ** ( 1 / exponente )
+        argumento = complejo.argumento() / exponente
+        complejos = []
+        for i in range( exponente ):
+            complejos.append( Complejo.CrearFormaPolar( modulo, argumento + math.pi * 2 / exponente * i ) )
+        return complejos
