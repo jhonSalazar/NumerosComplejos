eaf60fc664bfbd7fb2ccb7e604b823275fb22b98
Jhon Salazar <farsalazar@gmail.com>
2019-05-27 02:11:55 -0300

    se agrega funcionalidad de fasores

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import cmath 
 import math 
-# Prueba de script, inicializando un numero complejo
-x = 5
-y = 3
-
-z = complex(x,y); # creamos un numero complejo
-
-print("Numero compleo: ",z)
-print ("Parte Real:",end="") 
-print (z.real) 
-
-print ("Parte imaginaria:",end="") 
-print (z.imag)
+from paquetes.fasor import Fasor
+from paquetes.operaciones import Operaciones
+# Prueba de script, resultado de fasor
+         
+fasor1= Fasor(7,5,math.pi/3)
+fasor2= Fasor(5,5,3*math.pi/2)
+fasorResultado = Operaciones.sumarfasores(fasor1,fasor2)
+print ("SUMA DE FASORES : Amplitud, frecuencia, angulo inicial" )
+print("Amplitud: ",fasorResultado.amplitud,"Frecuencia: ", fasorResultado.frecuencia,"Angulo en Radianes",fasorResultado.anguloInicial)
